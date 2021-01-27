a75261b9394b97da159f79bfc08e4817983a3bcd
carlsus <carlsus@gmail.com>
2021-01-27 16:29:49 +0800

    Initial

---
+++ OrderProject/OrderProject/Models/OrderContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OrderProject.Models
+{
+    public class OrderContext:DbContext
+    {
+        public OrderContext():base("default")
+        {
+            
+        }
+    }
+}
